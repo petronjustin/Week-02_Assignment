e8a4245680164641c05aaddce4c743bdeae5e965
Justin Petron <justinpetron@Justins-MBP-2.lan>
2024-03-23 16:22:00 -0500

    first commit

---
+++ Loops_Week2.java
@@ -0,0 +1,35 @@
+package com.promineotech;
+
+public class Loops_Week2 {
+
+	public static void main(String[] args) {
+//		for (int i = 0; i < 10; i++) {
+//			System.out.println(i);
+		//}
+		
+//		for (int i = 1; i <= 10; i++) {
+//			System.out.println(i);
+	//	}
+		
+//		for (int i = 0; i < 100; i++) {  //You could also do i +=2 to increment by two.
+	//		if (i % 2 == 0) {
+//			System.out.println(i);    
+			
+//			}
+			
+//		}
+		int x = 10;
+		while (x < 10) {
+			System.out.println(x);
+			x++;
+		}
+			
+	//Do While loops always iterate at least once. Checks iteration at the end instead of beginning of the loop. While goes at the end.
+		do {
+			System.out.println(x);
+			x++;
+		} while (x < 2);
+		
+		
+	}
+}
